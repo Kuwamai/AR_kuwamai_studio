bf0529036eb32feaa6b6c6794a2e2aa719b0add5
Kuwamai <mitsumex333@gmail.com>
2020-05-06 19:42:59 +0900

    add: UnityCapture and edit object pose

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -12,23 +12,31 @@ public class Shoot : MonoBehaviour {
     public SteamVR_Input_Sources hand;
     public SteamVR_Action_Boolean clickAction;
 
+    public float timeOut;
+    private float timeElapsed;
+
     void Update () {
         if (clickAction.GetState(hand)){
+            timeElapsed += Time.deltaTime;
 
-            // 弾丸の複製
-            GameObject bullets = Instantiate(bullet) as GameObject;
-            Renderer coloring = bullets.GetComponent<Renderer>();
-            coloring.material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+            if(timeElapsed >= timeOut){
+                // 弾丸の複製
+                GameObject bullets = Instantiate(bullet) as GameObject;
+                Renderer coloring = bullets.GetComponent<Renderer>();
+                coloring.material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
 
-            Vector3 force;
+                Vector3 force;
 
-            force = muzzle.gameObject.transform.forward * speed;
+                force = muzzle.gameObject.transform.forward * speed;
 
-            // Rigidbodyに力を加えて発射
-            bullets.GetComponent<Rigidbody>().AddForce(force);
+                // Rigidbodyに力を加えて発射
+                bullets.GetComponent<Rigidbody>().AddForce(force);
 
-            // 弾丸の位置を調整
-            bullets.transform.position = muzzle.position;
+                // 弾丸の位置を調整
+                bullets.transform.position = muzzle.position;
+
+                timeElapsed = 0.0f;
+            }
         }
     }
 }
